d3a4c59a34ecfa8fa7227e7816b740093b6f711d
walviz <115000755+walviz@users.noreply.github.com>
2023-11-03 09:38:41 -0500

    inicializated

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const link = "http://passwordarris.somee.com/api/fecha/2024-01-01T00:00:00";
+
+var result = fetch (link).then(Response=> Response.text()).then(data=>console.log(data))
+
+//http://passwordarris.somee.com/api/fecha/2024-01-01T00:00:00
+//http://www.passwordarris.somee.com/api/fecha/2024-01-01T00:00:00
